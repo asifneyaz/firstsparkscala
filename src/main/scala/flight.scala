7aae76ecbaae8079e49c8b9fabd38bea82b4b5bf
[asifneyaz] <asifneyaz@gmail.com>
2021-08-10 10:17:10 -0500

    initial

--- src/main/scala/flight.scala
+++ src/main/scala/flight.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions
 object flight {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
@@ -12,6 +13,11 @@ object flight {
       .option("header", "true")
       .csv("data/flight-data/csv/2015-summary.csv")
     flightdata.show(3)
+    flightdata.createOrReplaceTempView("flight_data_view")
+    val sqlway = spark.sql(""" SELECT DEST_COUNTRY_NAME, COUNT(1) FROM flight_data_view GROUP BY DEST_COUNTRY_NAME""")
+    sqlway.show(100)
+    val maxcount = spark.sql("""SELECT max(COUNT) FROM flight_data_view""")
+    maxcount.show(1)
   }
 
 }
